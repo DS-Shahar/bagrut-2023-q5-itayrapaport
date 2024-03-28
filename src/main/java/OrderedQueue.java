653bc7bf98a04fd6847595ebcdf1b0687f130e1d
itayrapaport <126421641+itayrapaport@users.noreply.github.com>
2024-03-28 21:01:04 +0200

    Update OrderedQueue.java

--- src/main/java/OrderedQueue.java
+++ src/main/java/OrderedQueue.java
@@ -1,21 +1,102 @@
 
-public class OrderedQueue {
 
-	private Queue<NumCount> q;
-	
-	public OrderedQueue() {
-		q = new Queue<NumCount>();
-	}
+public class OrderedList {
+    private Queue<NumCount> lst;
 
-	public void insertNum(int x) {
-		
-	}
-	
-	public int valueN(int n) {
-		return -1;
-	}
-	
-	public void removeNum(int x) {
-		
-	}
+    public OrderedList() {
+        lst = new Queue<>();
+    }
+
+    public void insertNum(int x) {
+        if (lst.isEmpty()) {
+            lst.insert(new NumCount(x, 1));
+            return;
+        }
+
+        Queue<NumCount> tempQueue = new Queue<>();
+        NumCount currentNumCount = lst.remove();
+        while (currentNumCount != null) {
+            if (currentNumCount.getNum() == x) {
+                currentNumCount.setCount(currentNumCount.getCount() + 1);
+                break;
+            } else if (currentNumCount.getNum() > x) {
+                tempQueue.insert(new NumCount(x, 1));
+                tempQueue.insert(currentNumCount);
+                break;
+            } else {
+                tempQueue.insert(currentNumCount);
+                currentNumCount = lst.remove();
+            }
+        }
+
+        if (currentNumCount == null) {
+            tempQueue.insert(new NumCount(x, 1));
+        }
+
+        while (!lst.isEmpty()) {
+            tempQueue.insert(lst.remove());
+        }
+
+        lst = tempQueue;
+    }
+
+    public int valueN(int n) {
+        int sum = 0;
+        Queue<NumCount> tempQueue = new Queue<>();
+        NumCount result = null;
+
+        while (!lst.isEmpty()) {
+            NumCount currentNumCount = lst.remove();
+            sum += currentNumCount.getCount();
+            if (sum >= n) {
+                result = currentNumCount;
+                break;
+            }
+            tempQueue.insert(currentNumCount);
+        }
+
+        while (!tempQueue.isEmpty()) {
+            lst.insert(tempQueue.remove());
+        }
+
+        if (result != null) {
+            return result.getNum();
+        } else {
+            return -1;
+        }
+    }
+
+    public void removeNum(int x) {
+        Queue<NumCount> tempQueue = new Queue<>();
+        boolean found = false;
+
+        while (!lst.isEmpty()) {
+            NumCount currentNumCount = lst.remove();
+            if (currentNumCount.getNum() == x) {
+                if (currentNumCount.getCount() > 1) {
+                    currentNumCount.setCount(currentNumCount.getCount() - 1);
+                    tempQueue.insert(currentNumCount);
+                }
+                found = true;
+                break;
+            } else {
+                tempQueue.insert(currentNumCount);
+            }
+        }
+
+        while (!lst.isEmpty()) {
+            tempQueue.insert(lst.remove());
+        }
+
+        if (found) {
+            while (!tempQueue.isEmpty()) {
+                lst.insert(tempQueue.remove());
+            }
+        } else {
+            // If the number to remove is not found, reconstruct the original list
+            while (!tempQueue.isEmpty()) {
+                lst.insert(tempQueue.remove());
+            }
+        }
+    }
 }
